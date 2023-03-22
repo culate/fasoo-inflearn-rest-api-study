492d0c645ac0a7ac321815d10ffd19df9309cc33
culate <culate@FS0041C.fasoo.com>
2023-03-22 22:02:18 +0900

    매개변수를 이용한 테스트

--- demo/src/test/java/com/example/demo/events/EventControllerTests.java
+++ demo/src/test/java/com/example/demo/events/EventControllerTests.java
@@ -8,16 +8,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDateTime;
 
-import org.hamcrest.Matchers;
+//import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+//import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
--- demo/src/test/java/com/example/demo/events/EventTest.java
+++ demo/src/test/java/com/example/demo/events/EventTest.java
@@ -3,14 +3,19 @@ package com.example.demo.events;
 //import static org.assertj.core.api.Assertions.assertThat;
 
 //import org.junit.Test;
-import org.junit.jupiter.api.Test;
-import org.junit.runners.Parameterized.Parameters;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runners.Parameterized.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Assertions;
-//import junitparams.Parameters;
+//import org.junit.jupiter.api.Assertions;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
 	@Test
@@ -37,62 +42,49 @@ public class EventTest {
 	
     @Test
     @Parameters
-    public void testFree(/*int basePrice, int maxPrice, boolean isFree*/) {
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
         // Given
         Event event = Event.builder()
-                //.basePrice(basePrice)
-                //.maxPrice(maxPrice)
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         // When
         event.update();
 
         // Then
-        //assertThat(event.isFree()).isEqualTo(isFree);
-        assertThat(event.isFree()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
 
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        //assertThat(event.isFree()).isEqualTo(isFree);
-        assertThat(event.isFree()).isFalse();
-}
+    private Object[] parametersForTestFree() {
+        return new Object[] {
+            new Object[] {0, 0, true},
+            new Object[] {100, 0, false},
+            new Object[] {0, 100, false},
+            new Object[] {100, 200, false}
+        };
+    }
 
     @Test
     @Parameters
-    public void testOffline(/*String location, boolean isOffline*/) {
+    public void testOffline(String location, boolean isOffline) {
         // Given
         Event event = Event.builder()
-                //.location(location)
-        		.location("강남역 네이버 D2 스타텁 팩토리")
+                .location(location)
                 .build();
 
         // When
         event.update();
 
         // Then
-        //assertThat(event.isOffline()).isEqualTo(isOffline);
-        assertThat(event.isOffline()).isTrue();
-
-        // Given
-        event = Event.builder()
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        //assertThat(event.isOffline()).isEqualTo(isOffline);
-        assertThat(event.isOffline()).isFalse();
+        assertThat(event.isOffline()).isEqualTo(isOffline);
     }
 
+    private Object[] parametersForTestOffline() {
+        return new Object[] {
+            new Object[] {"강남", true},
+            new Object[] {null, false},
+            new Object[] {"       ", false}
+        };
+    }
 }
