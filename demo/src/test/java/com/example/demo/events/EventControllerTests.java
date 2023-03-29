5027e7fd9f0125c56cf3e2eb390cba001b3febff
culate <culate@FS0041C.fasoo.com>
2023-03-29 16:00:32 +0900

    테스트용 DB와 설정 분리하기

--- demo/src/test/java/com/example/demo/events/EventControllerTests.java
+++ demo/src/test/java/com/example/demo/events/EventControllerTests.java
@@ -36,6 +36,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -49,6 +50,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
 	@Autowired
